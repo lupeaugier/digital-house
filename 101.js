c42e16e03ace5c4736dcd05724efb433dbffa7c9
lupeaugier <45998867+lupeaugier@users.noreply.github.com>
2020-08-21 11:36:30 -0400

    Ejercicios Clase
     Repaso de funciones, condicionales y Arrays

---
+++ 101.js
@@ -0,0 +1,57 @@
+// Variables
+ 
+let primera = 0;
+var segunda = 0;
+const tercera = 0;
+
+// Una variable es...
+var variable = "Es un espacio donde almacenamos distintos tipos de datos";
+
+// Tipos de datos
+
+var numero = 23; // tipo de dato numerico
+var string = "Lupe"; // cadena de texto
+var booleano = true; // tipo de dato true o false
+var array = [ 1, "gato", true]; // almacena distitntos tipos de datos
+
+// tambien estan los objetos literales pero no se si se considera un tipo de dato porque se pueden almacenar en variables pero su estructura es mas compleja
+
+
+
+// console.log()
+// Sirve para mostrar en consola lo que estamos programando, que basicamente sirve como testeo para ver si lo que escribiste te da el resutlado que esperas
+
+
+// Ejecución con Node
+// Entiendo la definicion pero me es dificil entenderlo en la practica dado que no puedo comparar cuando no se usaba.
+// Lo que entiendo es que es un traductor de codigo para que todos lo puedan entender y tener la misma respuesta en todos los navegadores.
+
+// Operadores
+// Sirven para manipular variables.
+// Hay varios tipos, si queremos asignar =, los aritmeticos /*+-%++, comparacion == != > < 
+// logicos && || !, concatenacion + 
+
+// Operadores Aritméticos
+// Operadores de comparación
+// Operadores lógicos
+//   /|\
+//    |
+//    |
+//    |
+
+// Condicionales
+// sirven para evaluar si se cumplen condiciones
+//if / else if
+
+// Bucles
+// Permiten programar tareas repetitivas
+// For / While / Do While
+
+// For
+// Es lo mas complejo de entender..
+
+// Funciones
+// Sirven para programar acciones, que una vez declaradas las podes reutilizar
+function suma(numA, numB){
+    return numA+numB;
+}
